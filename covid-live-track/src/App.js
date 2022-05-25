b629c6ad32f34f890386670d068520ae3d2a9275
panditprogrammer <panditprogrammer@gmail.com>
2022-05-25 15:18:49 +0530

    fetch api - useState

---
+++ covid-live-track/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from './components/Head'
+import Status from './components/Status'
+
+function App() {
+  return (
+    <>
+     <Head></Head>
+     <Status></Status>
+    </>
+  )
+}
+
+export default App
---
+++ covid-live-track/src/components/Head.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Head() {
+    return (
+        <div className="head">
+            <h1>🔴 Live  </h1>
+            <h2>Help to stop Coronavirus - Covid-19 </h2>
+        </div>
+    )
+}
+
+export default Head
